36a4c727d079fc27fa04f78909b37669e3f6b738
murad <muradkv@gmail.com>
2019-03-09 22:03:25 +0300

    Add func random generateImage on UIImageView

--- ViewController.swift
+++ ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        generateImage()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        generateImage()
+    }
+    
+    func generateImage() {
+        
+        let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+        
+        let randomBallImage = Int.random(in: 0...4)
+        
+        ballImageView.image = UIImage.init(named: ballArray[randomBallImage])
+        
+    }
 }
 
